90beab74bc0b84d45bf4631d58fa85a4f5488b40
GabrielRF <github@gabrf.com>
2018-09-20 09:48:23 -0300

    Getting environment variables

--- v0.py
+++ v0.py
@@ -1,11 +1,17 @@
 import inotify.adapters
 import os
+import telebot
 
+TOKEN = os.getenv('BOT_TOKEN')
+FOLDER = os.getenv('FOLDER', '/tmp')
+EXTENSION = os.getenv('EXTENSION', 'mp4')
+DESTINATION = os.getenv('DESTINATION')
+
+bot = telebot.TeleBot(TOKEN)
 notifier = inotify.adapters.Inotify()
-notifier.add_watch('/tmp')
-extension = 'txt'
+notifier.add_watch(FOLDER)
 
 for event in notifier.event_gen():
     if event is not None:
-        if 'IN_CREATE' in event[1] and extension + '.' not in event[3]:
-            print(event[3])
+        if 'IN_CREATE' in event[1] and EXTENSION + '.' not in event[3]:
+            bot.send_animation(DESTINATION, event[3])
